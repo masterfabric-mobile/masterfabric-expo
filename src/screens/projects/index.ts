29e07626da35574635e48c4d3f628506afbaac8f
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-10-22 18:53:57 +0300

    feat: export ProjectsTabBar component for improved project navigation

--- src/screens/projects/index.ts
+++ src/screens/projects/index.ts
@@ -1,5 +1,6 @@
 export { ProjectCard } from './components/project-card';
 export { ProjectsScreen } from './components/projects-screen';
+export { ProjectsTabBar } from './components/projects-tab-bar';
 export { useProjectsViewModel } from './hooks/use-projects-view-model';
 export * from './models/project-models';
 export { useProjectsStore } from './store/projects-store';
