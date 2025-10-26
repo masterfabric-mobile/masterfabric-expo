6eba137cf0cec911a776c711f3251629d5d5d5d8
Sema Sert <semassertt@gmail.com>
2025-10-26 22:41:44 +0300

    feat: create index file for snackbar helper components, hooks, models, store, and utils

---
+++ src/screens/snackbar-helper/index.ts
@@ -0,0 +1,16 @@
+// Components
+export { SnackbarHelperScreen } from './components/snackbar-helper-screen';
+export { SnackbarInputField } from './components/snackbar-input-field';
+export { SnackbarTestCard } from './components/snackbar-test-card';
+
+// Hooks
+export { useSnackbarHelperViewModel } from './hooks/use-snackbar-helper-view-model';
+
+// Models
+export * from './models/snackbar-helper-models';
+
+// Store
+export { useSnackbarHelperStore } from './store/snackbar-helper-store';
+
+// Utils
+export * from './utils';
