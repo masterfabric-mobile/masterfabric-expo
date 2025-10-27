1d5599450dfecc63221a20b5f834aea663ec3cbe
Şevval Pamuk <130461759+sevvalpamuk@users.noreply.github.com>
2025-10-27 19:40:01 +0300

    refactor: simplify ToastHelperPage by removing ToastHelperProvider, directly rendering ToastHelperScreen

--- app/toast-helper.tsx
+++ app/toast-helper.tsx
@@ -1,9 +1,5 @@
-import { ToastHelperProvider, ToastHelperScreen } from '@/src/screens/toast-helper';
+import { ToastHelperScreen } from '@/src/screens/toast-helper';
 
 export default function ToastHelperPage() {
-  return (
-    <ToastHelperProvider>
-      <ToastHelperScreen />
-    </ToastHelperProvider>
-  );
+  return <ToastHelperScreen />;
 }
