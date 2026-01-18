9274116c336c5b77a3e5162eddb7e90e618ebbe1
Sema Sert <semassertt@gmail.com>
2026-01-18 18:41:41 +0300

    feat: introduce useWebViewer hook for enhanced web view management
    - Added a new useWebViewer hook to manage WebView state, security, and navigation.
    - The hook includes features for loading states, error handling, retries, and domain whitelisting.
    - This addition improves the application's capability to handle web content securely and efficiently.

--- packages/masterfabric-expo-core/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-
-export * from './useBatteryHelper';
-export * from './useMasterView';
-export * from './useOnboardingHelperViewModel';
-export * from './useVideoPlayerHelper';
-export * from './useHapticHelper';
