7fad4d4e11a490973059a74904d076927619157f
Sema Sert <semassertt@gmail.com>
2026-02-19 00:50:23 +0300

    feat: create index file for PermissionsHelper module
    - Added an index file to streamline exports for the PermissionsHelper module.
    - Included exports for the PermissionsHelperScreen, hooks, store, constants, models, and styles to facilitate easier imports throughout the application.

---
+++ src/screens/permissions-helper/index.ts
@@ -0,0 +1,7 @@
+export { PermissionsHelperScreen } from './components/permissions-helper-screen';
+export { usePermissionsHelperViewModel } from './hooks/use-permissions-helper-view-model';
+export { usePermissionsHelperStore } from './store/permissions-helper-store';
+export { PERMISSION_KEYS, CONFIG_PREVIEW_PERMISSIONS, PERMISSION_LABELS } from './constants/permissions-helper.constants';
+export type { PermissionKey } from './constants/permissions-helper.constants';
+export * from './models';
+export { permissionsHelperScreenStyles } from './styles/permissions-helper-screen.styles';
